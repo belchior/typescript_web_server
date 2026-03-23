b462467429c301f583631d1e873ba534bef23ff5
Belchior Oliveira <belchior@outlook.com>
2026-03-23 14:52:42 -0300

    improves client setup

---
+++ client/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import relay from 'vite-plugin-relay'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // plugins: [react({ babel: { plugins: ['relay'] } })],
+  plugins: [react(), relay],
+})
